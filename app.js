7493a8506a099fb753ccc50901847ccdefe640c1
axel <axel.bouillart@gmail.com>
2016-10-04 09:21:42 +0200

    exo color-pairing

--- app.js
+++ app.js
@@ -1,1 +1,81 @@
-// good luck !
+
+var color_in =$(".color");
+var len = color_in.length ;
+console.log(len);
+for (var i=0; i < len; i++) {
+	var font= $(color_in[i]).data("color");
+	$(color_in[i]).css("backgroundColor",font);
+}
+$(".color").click(function(){
+
+	if (checked() === true ){
+
+		$(".main").css("color",$(this).data("color"));
+		console.log("chinois")
+	} else {
+
+	$("body").css("backgroundColor",$(this).data("color"));
+	console.log("chinois")
+	}
+});
+function checked() {
+	return $("#modify-texte").is(":checked");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(".color").click(function(){
+// 	console.log(checked())
+// 	if (checked() === true){
+// 		$(".main").css("color",$(this).data("color"));
+// 	} else {
+// 		$("body").css("backgroundColor",$(this).data("color"));
+// 	}
+
+// });
