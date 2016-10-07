41ef96eb8dcb738c5a61ea6286af6abcb25cb73a
axel <axel.bouillart@gmail.com>
2016-10-07 12:25:01 +0200

    update color-pairing

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 
 var color_in =$(".color");
+
 var len = color_in.length ;
-console.log(len);
+
 for (var i=0; i < len; i++) {
+
 	var font= $(color_in[i]).data("color");
+
 	$(color_in[i]).css("backgroundColor",font);
+
 }
+
 $(".color").click(function(){
 
 	if (checked() === true ){
 
 		$(".main").css("color",$(this).data("color"));
-		console.log("chinois")
+	
+
 	} else {
 
-	$("body").css("backgroundColor",$(this).data("color"));
-	console.log("chinois")
+		$("body").css("backgroundColor",$(this).data("color"));
+		
 	}
 });
+
 function checked() {
+
 	return $("#modify-texte").is(":checked");
 }
 
